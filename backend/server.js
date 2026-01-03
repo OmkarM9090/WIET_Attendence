19fc987969e71d92e10b599d2217a6e1970c282f
unknown <omkarmahadik180@gmail.com>
2026-01-03 15:36:53 +0530

    auth controller registration function is updated

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 import app from "./src/app.js";
 
 dotenv.config();
-
-mongoose.connect(process.env.MONGO_URI).then(() =>
+try{
+  mongoose.connect(process.env.MONGO_URI).then(() =>
   console.log("MongoDB Connected")
 );
+}
+catch(err){
+  console.log("MongoDB connection error:", err);
+}
 
 app.listen(5000, () => console.log("Server running on port 5000"));
