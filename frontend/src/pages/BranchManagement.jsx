71a326402dc3e8341a5423a9df55e8341cc24f1e
unknown <omkarmahadik180@gmail.com>
2026-02-03 23:15:41 +0530

    branch management ui change

--- frontend/src/pages/BranchManagement.jsx
+++ frontend/src/pages/BranchManagement.jsx
@@ -428,7 +428,7 @@ export default function BranchManagement() {
 
           <div className="text-center">
             <p
-              className="text-sm"
+              className="text-sm font-medium"
               style={{ color: theme.colors.text.primary }}
             >
               Are you sure you want to delete the branch{" "}
